0d67190e5cc271bb6af59c84a3d3f172c46d27b5
odow <o.dowson@gmail.com>
2023-01-25 14:57:36 +1300

    Rethrow errors instead of returning FORCED_STOP

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ include("tutorial.jl")
 include("fix133.jl")
 include("MPB_wrapper.jl")
 include("MOI_wrapper.jl")
-include("exceptions.jl")
 
 using NLopt
 using Test
@@ -22,4 +21,36 @@ end
         "`initial_step(opt, x)` to access the initial step at a point `x`.",
     )
     @test_throws err opt.initial_step
+end
+
+@testset "Fix #156" begin
+    @testset "Test that SavedException is thrown" begin
+        f(x, g=[]) = (error("test error"); x[1]^2)
+        opt = Opt(:LN_SBPLX, 1)
+        opt.min_objective = f
+        @test_throws NLopt.SavedException{ErrorException} optimize(opt, [0.1234])
+        @test NLopt.nlopt_exception === nothing
+        try
+            optimize(opt, [0.1234])
+        catch e
+            # Check that the backtrace is being printed
+            @test length(sprint(show, e)) > 100
+        end
+    end
+    @testset "Test that ForcedStop does not rethrow" begin
+        f(x, g=[]) = (throw(NLopt.ForcedStop()); x[1]^2)
+        opt = Opt(:LN_SBPLX, 1)
+        opt.min_objective = f
+        fmin, xmin, ret = optimize(opt, [0.1234])
+        @test ret == :FORCED_STOP
+        @test NLopt.nlopt_exception === nothing
+    end
+    @testset "Test that no error works correctly" begin
+        f(x, g=[]) = (x[1]^2)
+        opt = Opt(:LN_SBPLX, 1)
+        opt.min_objective = f
+        fmin, xmin, ret = optimize(opt, [0.1234])
+        @test ret ∈ (:SUCCESS, :FTOL_REACHED, :XTOL_REACHED)
+        @test NLopt.nlopt_exception === nothing
+    end
 end
